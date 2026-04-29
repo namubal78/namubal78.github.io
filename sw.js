1b12789570eebab7911faa84892f3098310e881f
namubal78 <namubal78@users.noreply.github.com>
2026-04-29 08:33:10 +0000

    deploy: namubal78/Coking-Cooding@1381493ad85750f0fc7a097a2f46f9fef4804852

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'ck-v1'
+const PRECACHE = ['/', '/home', '/blog', '/portfolio', '/demo', '/login']
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  // API 요청은 캐시하지 않음
+  if (e.request.url.includes('/api/')) return
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE).then(c => c.put(e.request, clone))
+        }
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  )
+})
